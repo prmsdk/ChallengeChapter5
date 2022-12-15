570bd3c0a0cfd60b6c180015e4c5a93646306c10
prmsdk <dickayunia1@gmail.com>
2022-12-15 21:33:43 +0700

    Mengubah icon dan permission internet di manifest

--- app/src/main/java/com/example/challengechapter5/MainActivity.kt
+++ app/src/main/java/com/example/challengechapter5/MainActivity.kt
@@ -3,9 +3,11 @@ package com.example.challengechapter5
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
+import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,9 +23,13 @@ class MainActivity : AppCompatActivity() {
         val tvMainNama: TextView = findViewById(R.id.tv_main_name)
         val tvVsPlayer: TextView = findViewById(R.id.tv_vs_player)
         val tvVsCom: TextView = findViewById(R.id.tv_vs_com)
+        val ivVsPlayer: ImageView = findViewById(R.id.iv_vs_player)
 
         tvVsPlayer.text = valName + " VS Pemain"
         tvVsCom.text = valName + " VS Komputer"
+
+        Glide.with(this).load("https://lh3.googleusercontent.com/W6Gz9Qx5y6xHXNVMpM1OkhuxJS_IrLP627ZecWJ-d19VzEkXFHLPCrtboFjt4uctO6xo6BtQPKdlpbNTAOstravMNgd9a3La7LamCjtXyDx5UbmswEdnqCJFkho_H5VFPgDp0oMxRNMp8ojT5_G23IlTxU4UuGhVY0oxY8iGydvXIjVo-oxQfKKN_CQO5n4FkHfyh_lLcZBvbPCeCDQ3qRJiWZpXMoExLu90NOHQNAaJe-P98E2boOElr34o5UYYjQbSsIj5W8yvdQ1HJqxBipCOEnYAAFbRuEUvGnACyFI_oSP4Xq5k59AqB4MKExBSWjq4BrJp7WnszwFX_RyYE8v8WaIi3tkfZQBBeb2fHNzKlm5b0kNlfAnwFSrOSGV-7H9VLCiL7B1cc561qeJm-k_w-plG-bEG0Epu4pSSvnd_ydYRF0ywALTBKIHmt2I0Emx2y8Sl-5daym_nYqmxgeu8Re8VKEo_4XURpA-Rs87U116fK_fe6RgmxJOBKMEfwGmATenVvsnGn2kn3Ft3obL_-Kx1z7x7ChseYfDmC893M41_rgZkgEkQ7hJgluoVxe7QyQwCZsei5k57nEXWLpLj0mEosGo0vmLy4ZjD5rNJ9jtZbT52t9ZPUiuC2qdb46XqfeLQsc7wgs_tQLherbOnX2sadzWuzYbQPzv9MbXXB_deSrjPQhoMW3OZq-YT4Cp_bevH1H9HHToLRBZLAsgLcRAMNMFDBh2dueNc-wP90J4cef_ei1kAg_SspFKjtlLsDUnW_ZZ9N4vDPxNobukAXD7BaIdXUGIEqT7bdP4WcfCUPc-qWlKubKAUVdh4Xwt3EJ9omYY55BCXThSuUfyRQCTsEEOtgX3XQHx4Ak7FIq-GoKRcVrobTFsxa6VDbq_Gn-XU0jan17PLvP7Ma0RffqXuc_m5-hvHPC1voKTgtKZ-mxE-QzYaYfFets5UtoLzjK_2bCVU_DBpkngAP6z1y-yjR7No94tHcALrVU3Xu332PtmM19apRN5jSkt_Yk_-oRDV7qgZ75bpd451SscpEABPZKXZw7ak2bOXaR5f0SSs1Q8sZ0E=s512-no?authuser=0")
+            .into(ivVsPlayer)
 
         val snackbar = Snackbar.make(parentLayout, "Selamat Datang " + valName, Snackbar.LENGTH_LONG)
         snackbar.setAction("Tutup"){
